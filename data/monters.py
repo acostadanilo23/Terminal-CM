b22a46dff7ee2c86b703af4da9ca9ea5b3377ae6
acostadanilo <acostadanilo34@gmail.com>
2025-05-23 15:13:29 -0300

    add: added id for each monster

--- data/monters.py
+++ data/monters.py
@@ -1,185 +1,216 @@
 MONSTERS_DATA = {
     1: {
+        "id": 1,
         "name": "Vulto",
         "habilidade": 6,
         "energia": 6,
         "notas": "Invisível."
     },
     2: {
+        "id": 2,
         "name": "Aranha Gigante",
         "habilidade": 7,
         "energia": 6,
         "notas": "Em combate com duas delas, combata uma de cada vez."
     },
     3: {
+        "id": 3,
         "name": "Orc",
         "habilidade": 5,
         "energia": 5,
         "notas": ""
     },
     4: {
+        "id": 4,
         "name": "Esqueleto",
         "habilidade": 7,
         "energia": 5,
         "notas": ""
     },
     5: {
+        "id": 5,
         "name": "Guerreiro Zumbi",
         "habilidade": 7,
         "energia": 7,
         "notas": ""
     },
     6: {
+        "id": 6,
         "name": "Goblin",
         "habilidade": 5,
         "energia": 4,
         "notas": ""
     },
     7: {
+        "id": 7,
         "name": "Morcego Gigante",
         "habilidade": 6,
         "energia": 4,
         "notas": ""
     },
     8: {
+        "id": 8,
         "name": "Vampiro",
         "habilidade": 8,
         "energia": 8,
         "notas": "Se Energia < 4, tenta se transformar em Morcego para fugir (Parágrafo 159)."
     },
     9: {
+        "id": 9,
         "name": "Verme da Lama",
         "habilidade": 7,
         "energia": 7,
         "notas": ""
     },
     10: {
+        "id": 10,
         "name": "Fantasma",
         "habilidade": 8,
         "energia": 8,
         "notas": "Se falhar num Teste de Sorte quando atacado, perde 1 Sorte. Se passar, Fantasma é destruído."
     },
     11: {
+        "id": 11,
         "name": "Guerreiro Esqueleto",
         "habilidade": 8,
         "energia": 6,
         "notas": ""
     },
     12: {
+        "id": 12,
         "name": "Homem-Lagarto",
         "habilidade": 6,
         "energia": 7,
         "notas": ""
     },
     13: {
+        "id": 13,
         "name": "Ogro",
         "habilidade": 7,
         "energia": 8,
         "notas": "Em combate com dois, combata um de cada vez."
     },
     14: {
+        "id": 14,
         "name": "Gárgula",
         "habilidade": 8,
         "energia": 8,
         "notas": ""
     },
     15: {
+        "id": 15,
         "name": "Gigante",
         "habilidade": 9,
         "energia": 10,
         "notas": ""
     },
     16: {
+        "id": 16,
         "name": "Homem-Porco",
         "habilidade": 7,
         "energia": 6,
         "notas": ""
     },
     17: {
+        "id": 17,
         "name": "Orc-Chefe",
         "habilidade": 7,
         "energia": 6,
         "notas": ""
     },
     18: {
+        "id": 18,
         "name": "Guerreiro Orc",
         "habilidade": 6,
         "energia": 5,
         "notas": ""
     },
     19: {
+        "id": 19,
         "name": "Vaporizador",
         "habilidade": 8,
         "energia": 7,
         "notas": "Se atingido, perde 1 ENERGIA extra por estar atordoado."
     },
     20: {
+        "id": 20,
         "name": "Centopeia Gigante",
         "habilidade": 6,
         "energia": 6,
         "notas": ""
     },
     21: {
+        "id": 21,
         "name": "Escorpião Gigante",
         "habilidade": 8,
         "energia": 7,
         "notas": ""
     },
     22: {
+        "id": 22,
         "name": "Criatura das Profundezas",
         "habilidade": 9,
         "energia": 9,
         "notas": ""
     },
     23: {
+        "id": 23,
         "name": "Monstro Espectro",
         "habilidade": 9,
         "energia": 10,
         "notas": "Se perder um Teste de Sorte quando atacado, perde 2 Sorte. Se passar, apenas 1."
     },
     24: {
+        "id": 24,
         "name": "Demônio da Cripta",
         "habilidade": 10,
         "energia": 10,
         "notas": ""
     },
     25: {
+        "id": 25,
         "name": "Morcego",
         "habilidade": 5,
         "energia": 4,
         "notas": "Aparece em bandos, mas o combate é com um por vez (Parágrafo 303)."
     },
     26: {
+        "id": 26,
         "name": "Lobisomem",
         "habilidade": 8,
         "energia": 9,
         "notas": "Imune a ataques normais. Precisa de espada de prata para dano total."
     },
     27: {
+        "id": 27,
         "name": "Minotauro",
         "habilidade": 9,
         "energia": 9,
         "notas": ""
     },
     28: {
+        "id": 28,
         "name": "Vigia-Chefe",
         "habilidade": 7,
         "energia": 7,
         "notas": ""
     },
     29: {
+        "id": 29,
         "name": "Vigia",
         "habilidade": 6,
         "energia": 6,
         "notas": ""
     },
-     30: {
+    30: {
+        "id": 30,
         "name": "Demônio Alado",
         "habilidade": 9,
         "energia": 8,
         "notas": ""
     },
     31: {
+        "id": 31,
         "name": "Barão Sukumvit",
         "habilidade": 12,
         "energia": 12,
